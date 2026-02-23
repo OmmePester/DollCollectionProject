81e1fd726eb6c87d725a75a6bed7a6cd46dab582
OmmePester <osmanapp12@gmail.com>
2026-02-23 03:17:55 +0300

    commit all v3 sql, we now have 8 parameters dor Doll! Sql connections are arranged, but needs bug check.

--- src/main/java/com/example/dollcollectionproject/model/Doll.java
+++ src/main/java/com/example/dollcollectionproject/model/Doll.java
@@ -2,40 +2,60 @@ package com.example.dollcollectionproject.model;
 
 public class Doll {
     // VARIABLES
-    private String name;
-    private int year;
-    private String brand;
-    private String imagePath;
-    // Extra variables that will be added by user
-    private String description;    // user will add and delete it manually
     private int id;    // added for sql stuff
+    private String imagePath;
+    private String name;
+    private String hint;
+    private String description;
+    private String brand;
+    private String model;
+    private int year;
+    // Extra variables that will be added by user
 
     // CONSTRUCTOR: How we create a new Doll
-    public Doll(String name, int year, String brand, String imagePath, int id) {
-        this.name = name;
-        this.year = year;
-        this.brand = brand;
-        this.imagePath = imagePath;
-        this.description = "";    // empty by default
+    public Doll(int id, String imagePath, String name, String hint, String description, String brand, String model, int year) {
         this.id = id;
+        this.imagePath = imagePath;
+        this.name = name;
+        this.hint = hint;
+        this.description = description;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
     }
 
     // GETTERS: Used by JavaFX to display the data
-    public String getName() { return name; }
-    public int getYear() { return year; }
-    public String getBrand() { return brand; }
-    public String getImagePath() { return imagePath; }
-    public String getDescription() { return description; }
     public int getId() { return id; }
-
-    // SETTER: Allows the user to edit the description
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    public String getImagePath() { return imagePath; }
+    public String getName() { return name; }
+    public String getHint() { return hint; }
+    public String getDescription() { return description; }
+    public String getBrand() { return brand; }
+    public String getModel() { return model; }
+    public int getYear() { return year; }
 
     // DELETER: Clears the description
     public void deleteDescription() {
         this.description = "";
-    }
+    }//MAY DELETE LATER AS NO USE
 
+    // SETTER: Allows the user to edit the description
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setHint(String hint) {
+        this.hint = hint;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
